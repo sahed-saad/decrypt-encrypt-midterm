c7e9dda9a3c32f465f43e54c31b3c34620b20601
Sahed Saad <158004018+sahed-saad@users.noreply.github.com>
2025-10-31 09:35:55 -0400

    Refactor main function for encryption and decryption

--- main.cpp
+++ main.cpp
@@ -4,9 +4,37 @@
 #include <cctype>
 using namespace std;
 
-int main(){
 
-    string cipherText = "TIPA MELZ EIBE LFYG HREI LSMS PSPT DRSJ ZNRA HBEE UIRU ZOQM GJYA SIAO JTGM NHTT BENE ZWGE TFGE QAEL YOVL OXEO NVDF CISX OQPR ESRN TTBE NGZL KSWB YAKC SJDB TXIU SARR OQZW DVOG LNSM CJAA SMNH LNXE NBWY SMCB WRDP AUTO MWOS ERTX HTTT RTRP GIMG EJDT NEST TSSY SJYM ZOIO RAUE IMLB KIWI LTVI ASPA KVEB OYZG QVLI MXEE HISL TIPA MELZ EIBE LFYG HREX PAUI SBWG DFRB TCOE TUPR MWJV DTZW TIPJ ZGQV LRCP OPXW DEVF DFKS WFCS ZRDM PAUI S";
+
+int main() {
+    
+    string phrase;
+  vector<int> keys;
+  string encryptedText = "";
+  string decryptedText = "";
+  int i;
+  int firstKey = 24;
+  int randomKey;
+  int previousKey;
+
+  unsigned long seed = time(0);     
+  unsigned long a = 1664525;         
+  unsigned long c = 1013904223;      
+  unsigned long long m = 4294967296;
+
+    string userName;
+    int userNum;
+    cout << "Hey " << "username" << "!";
+    cout << "This program has 2 main parts: i) Decryption and ii) Encryption." << endl;
+    cout << "For Part (i), Decryption of ciphertext that have been encrypted by Alice and sent to Bob";
+    cout << "If you would like to continue to view the decrypted text which we've done with our sophisticated algorithm, Enter (1)" << endl;
+    cout << "Else, if you would like to encrypt a text that you input,Part 1, we will encrypt with our encryption algorithm. For this part enter (2)" << endl;
+
+    cout << "Input either (1) or (2):";
+    cin >> userNum;
+
+    if (userNum == 1) {
+            string cipherText = "TIPA MELZ EIBE LFYG HREI LSMS PSPT DRSJ ZNRA HBEE UIRU ZOQM GJYA SIAO JTGM NHTT BENE ZWGE TFGE QAEL YOVL OXEO NVDF CISX OQPR ESRN TTBE NGZL KSWB YAKC SJDB TXIU SARR OQZW DVOG LNSM CJAA SMNH LNXE NBWY SMCB WRDP AUTO MWOS ERTX HTTT RTRP GIMG EJDT NEST TSSY SJYM ZOIO RAUE IMLB KIWI LTVI ASPA KVEB OYZG QVLI MXEE HISL TIPA MELZ EIBE LFYG HREX PAUI SBWG DFRB TCOE TUPR MWJV DTZW TIPJ ZGQV LRCP OPXW DEVF DFKS WFCS ZRDM PAUI S";
 
 // vector keys has a size of 83 elements, like possible keys for decryption
     vector<string>keys = {"utter", "descriptive", "caring", "pies", "drawer", "station", "soothe", "year", 
@@ -109,6 +137,109 @@ and then our first key becomes false;
 
     cout << "The correct English plain text: " << finalText << endl;
     cout << "Best key: " << correctKey << endl;
+
+    return 0;
+
+    }
+
+    else if (userNum == 2) {
+          
+
+
+  cout << "Enter the phrase you would like to encrypt: " << endl;
+  getline(cin, phrase);
+
+
+  keys.resize(phrase.size());
+
+  for (i = 0; i < phrase.size(); i++){
+    if (isalpha(phrase.at(i))) 
+    {
+      if (isupper(phrase.at(i))) 
+      {
+        if(i == 0)
+        {
+          previousKey = firstKey;
+        }
+        else
+        {
+          previousKey = keys.at(i - 1);
+        }
+
+        seed = (a * seed + c) % m;
+        unsigned long mixed = seed + (phrase.at(i) * i * 42) + previousKey;
+        randomKey = mixed % 26;
+        keys.at(i) = randomKey;
+        encryptedText.push_back((phrase.at(i) - 'A' + randomKey) % 26 + 'A');
+      } 
+      else if (islower(phrase.at(i))) 
+      {
+        if(i == 0)
+        {
+          previousKey = firstKey;
+        }
+        else
+        {
+          previousKey = keys.at(i - 1);
+        }
+
+        seed = (a * seed + c) % m;
+        unsigned long mixed = seed + (phrase.at(i) * i * 42) + previousKey;
+        randomKey = mixed % 26;
+        keys.at(i) = randomKey;
+        encryptedText.push_back((phrase.at(i) - 'a' + randomKey) % 26 + 'a');
+      }
+    } 
+    else 
+    {
+
+      encryptedText.push_back(phrase.at(i));
+    }
+  }
+
+  cout << "Encrypted Text: " << encryptedText << endl << "Keys: ";
+  for(i = 0; i < keys.size(); i++){
+    if(i == keys.size() - 1)
+    {
+        cout << keys.at(i);
+    }
+    else
+    {
+        cout << keys.at(i) << ", ";
+    }
+  }
+
+  for (i = 0; i < encryptedText.size(); i++) 
+  {
+
+    if (isalpha(encryptedText.at(i))) 
+    {
+
+      if (isupper(encryptedText.at(i))) 
+      {
+        decryptedText.push_back(((encryptedText.at(i) - 'A') - keys.at(i) + 26) % 26 + 'A');
+      } 
+      else if (islower(encryptedText.at(i))) 
+      {
+        decryptedText.push_back(((encryptedText.at(i) - 'a') - keys.at(i) + 26) % 26 + 'a');
+      }
+    } 
+    else 
+    {
+
+      decryptedText.push_back(encryptedText.at(i));
+    }
+  }
+
+  cout << endl << "Decrypted text: " << decryptedText << endl;
+
+  return 0;
+    }
+    else {
+        cout << "Your input is out of range" << endl;
+    }
+
+
 
     return 0;
 }
